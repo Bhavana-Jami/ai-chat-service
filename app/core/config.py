a28606482219fc255668285ab5873fd4ee9bd8ed
BhavanaJami <bhavanajami111@gmail.com>
2026-05-02 17:30:20 +0530

    cors update

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     openai_api_key: str
-    allowed_origins: list[str] = ["http://localhost:3000"]
+    allowed_origins: list[str] = ["http://localhost:5173/"]
     port: int = 8000                
 
     class Config:
